d408c9581d16449bd397d5b7f0481db013aa1466
darrickpang <dkypang@yahoo.com>
2021-04-21 19:43:03 -0700

    user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_secure_password
+
   has_many :dosages
   has_many :side_effects
   has_many :medicines, through: :side_effects
   has_many :medicines, through: :dosages
+
+  has_secure_password
+  has_secure_password :recovery_password, validations: false
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
