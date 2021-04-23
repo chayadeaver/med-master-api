ab6e4a43c70981473db2e8feaee2fe118995b029
chayadeaver <chayadeaver@gmail.com>
2021-04-22 23:33:03 -0400

    removed side_effects and add journals relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :dosages
-  has_many :side_effects
-  has_many :medicines, through: :side_effects
   has_many :medicines, through: :dosages
+  has_many :journals
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
