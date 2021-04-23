e41bd5c268c1812ea00585e1b2d3133771361057
chayadeaver <chayadeaver@gmail.com>
2021-04-22 23:31:14 -0400

    changed amount datatype to float

--- db/migrate/20210421153945_create_dosages.rb
+++ db/migrate/20210421153945_create_dosages.rb
@@ -3,9 +3,9 @@ class CreateDosages < ActiveRecord::Migration[6.1]
     create_table :dosages do |t|
       t.integer :user_id
       t.integer :medicine_id
-      t.integer :amount
-      t.datetime :startDate
-      t.datetime :endDate
+      t.float :amount
+      t.string :startDate
+      t.string :endDate
       
     end
   end
