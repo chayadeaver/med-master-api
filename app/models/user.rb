44f4d683388d80831dcc7c403108e88573d50919
darrickpang <dkypang@yahoo.com>
2021-04-24 18:14:23 -0700

    user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_password :recovery_password, validations: false
   has_many :dosages
   has_many :side_effects
   has_many :medicines, through: :side_effects
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       resources :medicines
     end
   end
-  
+  post '/user_login', to: 'users#login'
+  get '/user_persist', to: 'users#persist'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
